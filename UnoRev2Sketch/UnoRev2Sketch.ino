822259454b2564f0f6eabe6860c07561673c9f18
Alexandre Kneifel <alexandrekneifel@gmail.com>
2026-01-21 13:16:39 -0500

    Replaced files with up to date local files

--- UnoRev2Sketch/UnoRev2Sketch.ino
+++ UnoRev2Sketch/UnoRev2Sketch.ino
@@ -1,14 +1,19 @@
+/**
+*
+* This file contains control code for Arduino Rev 2.
+* It is the master in I2C to Uno Rev3, it controls all the motors (via 
+* a DC motor shield) : the DC motors, the servo motor via low switching.
+* It also monitors if a casette has been entered into the machine via two infrared sensors.
+*
+**/
+
 #include <Wire.h>
-#include <Adafruit_MotorShield.h>
+#include <AFMotor.h>
 #include <Servo.h>
 
 // Create motor shield object
-//TODO i dont think this is how i use motor shield
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-
-// Create motor objects
-Adafruit_DCMotor *motor1 = AFMS.getMotor(1); // M1
-Adafruit_DCMotor *motor4 = AFMS.getMotor(4); // M4
+AF_DCMotor motor1(1);
+AF_DCMotor motor4(4);
 
 // Servo object
 Servo cassetteServo;
@@ -16,27 +21,35 @@ const int SERVO_PIN = 9; // Servo 1
 const int SERVO_ENABLE = A0; // Enable pin
 
 // Infrared Sensors
-const int IR_SENSOR_1 = A2;
-const int IR_SENSOR_2 = A3;
+const int IR_SENSOR_FRONT = A2;
+const int IR_SENSOR_BACK = A3;
 
 // I2C Address of Slave Arduino
 // TODO i dont think this is my slave address
 const int SLAVE_ADDRESS = 0x08;
 
-// State tracking
-bool cassettePresent = false;
+enum State 
+{
+  IDLE,
+  CASSETTE_DETECTED_FRONT,
+  CASSETTE_READY,
+  CASSETTE_DETECTED_REAR
+};
+
+State currentState = IDLE;
+
+bool frontside = false;
+bool backside = false;
 
 void setup() 
 {
     Serial.begin(9600);
     Wire.begin();
 
-    // Initialize motor shield
-    AFMS.begin();
-
-    // Set motor speed to 255 (max speed)
-    motor1->setSpeed(255);
-    motor4->setSpeed(255);
+    motor1.setSpeed(0);
+    motor4.setSpeed(0);
+    motor1.run(RELEASE);
+    motor4.run(RELEASE);
 
     // Attach servo
     cassetteServo.attach(SERVO_PIN);
@@ -44,49 +57,138 @@ void setup()
     digitalWrite(SERVO_ENABLE, LOW); // Disable servo initially
 
     // Sensor setup
-    pinMode(IR_SENSOR_1, INPUT_PULLUP);
-    pinMode(IR_SENSOR_2, INPUT_PULLUP);
+    pinMode(IR_SENSOR_FRONT, INPUT_PULLUP);
+    pinMode(IR_SENSOR_BACK, INPUT_PULLUP);
+
 }
 
 void loop() 
 {
-    // Read infrared sensors
-    bool sensor1 = digitalRead(IR_SENSOR_1);
-    bool sensor2 = digitalRead(IR_SENSOR_2);
+  frontside = digitalRead(IR_SENSOR_FRONT);
+  backside = digitalRead(IR_SENSOR_BACK);
 
-    if (sensor1 == LOW && sensor2 == LOW && !cassettePresent) 
-    {
-        Serial.println("Cassette detected, driving motors...");
-        driveMotors();
+  Serial.println(frontside);
+  Serial.println(backside);
 
-        Wire.beginTransmission(SLAVE_ADDRESS);
-        Wire.write(0x01); // Cassette detected
-        Wire.endTransmission();
 
-        cassettePresent = true;
-    }
 
-    if (sensor1 == HIGH && sensor2 == HIGH && cassettePresent) 
-    {
-        Serial.println("Cassette on lift, stopping motors.");
+    // motor1.setSpeed(255);
+    // motor4.setSpeed(255);
+    // motor1.run(BACKWARD);
+    // motor4.run(FORWARD);
+
+  switch (currentState)
+  {
+    case IDLE:
+      if(frontside == LOW && backside == HIGH)
+      {
+        startRampUpMotors();
+        currentState = CASSETTE_DETECTED_FRONT;
+      }
+      else if(frontside == HIGH && backside == LOW)
+      {
+        startRampUpOuttakeMotors();
+        currentState = CASSETTE_DETECTED_REAR;
+      }
+      else
+      {
         stopMotors();
+      }
+      break;
 
-        Wire.beginTransmission(SLAVE_ADDRESS);
-        Wire.write(0x02); // Cassette on lift
-        Wire.endTransmission();
+    case CASSETTE_DETECTED_FRONT:
+      if(backside == LOW)
+      {
+        rampDownMotors();
+        currentState = CASSETTE_READY;
+      }
+      break;
 
-        cassettePresent = false;
+    case CASSETTE_READY:
+      if(backside == HIGH)
+      {
+        currentState = IDLE;
+      }
+      break;
+    
+    case CASSETTE_DETECTED_REAR:
+      if(frontside == HIGH && backside == HIGH)
+      {
+        rampDownOuttakeMotors();
+        currentState = IDLE;
+      }
+      break;
+  }
+
+
+
+}
+
+
+// could do a test on my motors to see when each starts spinning
+void startRampUpMotors() 
+{
+    motor1.setSpeed(255);
+    motor4.setSpeed(255);
+    motor1.run(FORWARD);
+    motor4.run(BACKWARD);
+    delay(100); // Short boost (adjust if needed)
+
+    // Now smoothly ramp up from 75 to max (255)
+    for (int speed = 75; speed <= 255; speed++) 
+    {
+        motor1.setSpeed(speed);
+        motor4.setSpeed(speed);
+        delay(10); // Smooth transition
     }
 }
 
-void driveMotors() 
+void rampDownMotors() 
 {
-    motor1->run(FORWARD);
-    motor4->run(FORWARD);
+    for (int speed = 255; speed >= 0; speed--) 
+    {
+        motor1.setSpeed(speed);
+        motor4.setSpeed(speed);
+        motor1.run(FORWARD); // Intake direction
+        motor4.run(BACKWARD);
+        delay(10);  // Ramp down delay
+    }
+    stopMotors();
+}
+
+void startRampUpOuttakeMotors() 
+{
+    motor1.setSpeed(255);
+    motor4.setSpeed(255);
+    motor1.run(BACKWARD);
+    motor4.run(FORWARD);
+    delay(100); // Short boost (adjust if needed)
+    
+    for (int speed = 75; speed <= 255; speed++) 
+    {
+        motor1.setSpeed(speed);
+        motor4.setSpeed(speed);
+        delay(10);  // Ramp up delay
+    }
+}
+
+void rampDownOuttakeMotors() 
+{
+    for (int speed = 255; speed >= 0; speed--) 
+    {
+        motor1.setSpeed(speed);
+        motor4.setSpeed(speed);
+        motor1.run(BACKWARD); // Intake direction
+        motor4.run(FORWARD);
+        delay(10);  // Ramp down delay
+    }
+    stopMotors();
 }
 
 void stopMotors() 
 {
-    motor1->run(RELEASE);
-    motor4->run(RELEASE);
+    motor1.run(RELEASE);
+    motor4.run(RELEASE);
+    motor1.setSpeed(0);
+    motor4.setSpeed(0);
 }
--- UnoRev3Sketch/UnoRev3Sketch.ino
+++ UnoRev3Sketch/UnoRev3Sketch.ino
@@ -1,6 +1,6 @@
 /**
 * This is the running code for Arduino Rev3.
-* This arduino receives messages from Raspberry Pi on UART, receives messages from arduino Rev2 on I2C,
+* This arduino receives messages from RaspberrX Pi on UART, receives messages from arduino Rev2 on I2C,
 * monitors stepper motor limit switches, and controls the three stepper motors.
 *
 **/
@@ -11,22 +11,27 @@
 /**
 *
 * When should I put the stepper motor boards in sleep mode ?
-* I need to defined my bytes from raspberry pi to this arduino
-* I need to define my bytes from teh toher arduino to this one
+* I need to defined mX bXtes from raspberrX pi to this arduino
+* I need to define mX bXtes from teh toher arduino to this one
 * Do i need micro stepping ?
 *
 **/
 #include <EnableInterrupt.h>
 #include <Wire.h>
+#include <AccelStepper.h>
+#include <AccelStepperWithDistance.h>
 
 //limit switches
+//green/brown
 const int Z_TOP_LIMIT = A0;
+//red/blue
 const int Z_BTM_LIMIT = A1;
-const int Y_LEFT_LIMIT = A2;
-const int Y_RIGHT_LIMIT = A3;
+const int X_RIGHT_LIMIT = A2;
+const int X_LEFT_LIMIT = A3;
 
 //stepper motors
 const int LEFT_Z_MTR_EN = 13;
+//SLP will be good for when nothing is actively happening
 const int LEFT_Z_MTR_SLP = 12;
 const int LEFT_Z_MTR_STEP = 11;
 const int LEFT_Z_MTR_DIR = 10;
@@ -36,44 +41,117 @@ const int RIGHT_Z_MTR_SLP = 8;
 const int RIGHT_Z_MTR_STEP = 7;
 const int RIGHT_Z_MTR_DIR = 6;
 
-const int Y_MTR_EN = 5;
-const int Y_MTR_SLP = 4;
-const int Y_MTR_STEP = 3;
-const int Y_MTR_DIR = 2;
+const int X_MTR_EN = 5;
+const int X_MTR_SLP = 4;
+const int X_MTR_STEP = 3;
+const int X_MTR_DIR = 2;
 
-void killMotors();
-void receiveData(int byteCount);
+// spacing between the shelves
+// should be able to do 0.02 mm steps
+const double Z_SHELF_DIST = 25.65;
+// should be able to do 0.1 mm steps
+const double X_SHELF_DIST = 104.5;
+// pos of entrance from limit switch
+// Z distance is unique, doesn't need to be perfect just above and below as necessary
+// 1 is the minimum but it is rubbing and shifting the cassette
+const double ENTR_Z_DIST = 0;
+// same as middle shelf with offset
+//213 is too far right
+const double ENTR_X_DIST = 211;
+// distance from limit switch to the first shelf
+// this could maybe be less
+const double X_OFFSET = 3;
+const double Z_OFFSET = 0;
+// number of shelves
+const int TOP_SHELF_IDX = 11;
+const int RIGHT_SHELF_IDX = 4;
+
+// from the above, homing sequence seems to put as at the right spot to pick up the casette
+// spacing for the shelves seems to be good though, no Z offset necessary
+
+volatile bool topLimLatched = false;
+volatile bool btmLimLatched = false;
+volatile bool leftLimLatched = false;
+volatile bool rightLimLatched = false;
+
+bool topLimHit = false;
+bool btmLimHit = false;
+bool leftLimHit = false;
+bool rightLimHit = false;
+
+AccelStepperWithDistance leftZStepper(AccelStepperWithDistance::DRIVER, LEFT_Z_MTR_STEP, LEFT_Z_MTR_DIR);
+AccelStepperWithDistance rightZStepper(AccelStepperWithDistance::DRIVER, RIGHT_Z_MTR_STEP, RIGHT_Z_MTR_DIR);
+AccelStepperWithDistance xStepper(AccelStepperWithDistance::DRIVER, X_MTR_STEP, X_MTR_DIR);
 
 struct Position
 {
-  float ypos;
-  float zpos;
-}
+  double xPos;
+  double zPos;
+};
 
-// TODO figure out home position
-const Position home_pos = {0,0}
+const Position entrancePos = {ENTR_X_DIST, ENTR_Z_DIST} ;
+
+void killMotors();
+Position posTranslation(int xIdx, int zIdx);
+void receiveData(int byteCount);
+void enableMotorsZ();
+void disableMotorsZ();
+void enableMotorsX();
+void disableMotorsX();
+void moveTo(Position posToGoTo);
+
+
+// // TODO figure out home position
+// const Position home_pos = {0,0}
 
 void setup() 
 {
 
-// setting up serial comm with the raspberry pi
-Serial1.begin(9600);
+Serial.begin(9600);
+Serial.println("Serial started");
+// setting up serial comm with the raspberrX pi
+//Serial1.begin(9600);
 
 //setting up i2c communication
-Wire.begin(0);
-Wire.onReceive(receiveData);
+// Wire.begin(0);
+// Wire.onReceive(receiveData);
 // slave will have address 0
+
 
 // limit switches for the stepper motors
 pinMode(Z_TOP_LIMIT, INPUT_PULLUP);
 pinMode(Z_BTM_LIMIT, INPUT_PULLUP);
-pinMode(Y_LEFT_LIMIT, INPUT_PULLUP);
-pinMode(Y_RIGHT_LIMIT, INPUT_PULLUP);
+pinMode(X_LEFT_LIMIT, INPUT_PULLUP);
+pinMode(X_RIGHT_LIMIT, INPUT_PULLUP);
 
 enableInterrupt(Z_TOP_LIMIT, killMotors, FALLING);
 enableInterrupt(Z_BTM_LIMIT, killMotors, FALLING);
-enableInterrupt(Y_LEFT_LIMIT, killMotors, FALLING);
-enableInterrupt(Y_RIGHT_LIMIT, killMotors, FALLING);
+enableInterrupt(X_LEFT_LIMIT, killMotors, FALLING);
+enableInterrupt(X_RIGHT_LIMIT, killMotors, FALLING);
+
+leftZStepper.setMaxSpeed(1500);
+// was 500
+leftZStepper.setAcceleration(1000);
+leftZStepper.setStepsPerRotation(400);  // For a 1.8° stepper motor, it's 200 steps
+leftZStepper.setDistancePerRotation(8); // If one rotation moves 8mm
+
+// was 1000, 500
+rightZStepper.setMaxSpeed(1500);
+rightZStepper.setAcceleration(1000);
+rightZStepper.setStepsPerRotation(400);  // For a 1.8° stepper motor, it's 200 steps
+rightZStepper.setDistancePerRotation(8); // one rotation moves 8mm
+
+xStepper.setMaxSpeed(1500);
+xStepper.setAcceleration(1000);
+xStepper.setStepsPerRotation(400);  // For a 1.8° stepper motor, it's 200 steps
+// distance per rotation will be different for a timing belt
+// pitch of 2mm/tooth with 20 teeth on the circumference
+xStepper.setDistancePerRotation(40); 
+
+//invert direction
+// I dont think i need this
+// leftZStepper.setPinsInverted(false, true);  
+// rightZStepper.setPinsInverted(false, true);
 
   //stepper motor setup
 pinMode(LEFT_Z_MTR_EN, OUTPUT);
@@ -82,85 +160,321 @@ pinMode(LEFT_Z_MTR_DIR, OUTPUT);
 pinMode(LEFT_Z_MTR_STEP, OUTPUT);
 
 // low means stepper driver board is enabled
-digitalWrite(LEFT_Z_MTR_EN, LOW);
+digitalWrite(LEFT_Z_MTR_EN, HIGH);
 // high means the stepper driver board is not in sleep mode
 digitalWrite(LEFT_Z_MTR_SLP, HIGH);
 
+//Right Z stepper motor setup
 pinMode(RIGHT_Z_MTR_EN, OUTPUT);
 pinMode(RIGHT_Z_MTR_SLP, OUTPUT);
 pinMode(RIGHT_Z_MTR_DIR, OUTPUT);
 pinMode(RIGHT_Z_MTR_STEP, OUTPUT);
 
-digitalWrite(RIGHT_Z_MTR_EN, LOW);
+digitalWrite(RIGHT_Z_MTR_EN, HIGH);
 digitalWrite(RIGHT_Z_MTR_SLP, HIGH);
 
-pinMode(Y_MTR_EN, OUTPUT);
-pinMode(Y_MTR_SLP, OUTPUT);
-pinMode(Y_MTR_DIR, OUTPUT);
-pinMode(Y_MTR_STEP, OUTPUT);
+// X motor stepper setup
+pinMode(X_MTR_EN, OUTPUT);
+pinMode(X_MTR_SLP, OUTPUT);
+pinMode(X_MTR_DIR, OUTPUT);
+pinMode(X_MTR_STEP, OUTPUT);
 
-digitalWrite(Y_MTR_EN, LOW);
-digitalWrite(Y_MTR_SLP, HIGH);
+digitalWrite(X_MTR_EN, HIGH);
+digitalWrite(X_MTR_SLP, HIGH);
 
 
 }
 
 void loop() 
 {
-  // Receving Raspberry Pi Serial
-  if (Serial1.available()) 
+
+// checking if switch is being held still
+
+  topLimHit = (digitalRead(Z_TOP_LIMIT) == LOW);
+  btmLimHit = (digitalRead(Z_BTM_LIMIT) == LOW);
+  leftLimHit = (digitalRead(X_LEFT_LIMIT) == LOW);
+  rightLimHit = (digitalRead(X_RIGHT_LIMIT) == LOW);
+
+// checking the interrupt condition to kill motors
+// TODO :maybe motor getting killed should be in the interrupt so it doesnt have to go thru some conditions before getting here
+  if(topLimLatched || btmLimLatched || leftLimLatched || rightLimLatched)
   {
-    // i should come up with a hex encoding system for my data
-    byte incomingByte = Serial1.read();  // Read one byte from Serial1
+    disableMotorsZ();
+    Serial.println("Z Motor's killed");
 
-    // Optional: Print the received byte for debugging
-    Serial.print("Received byte: ");
-    Serial.println(incomingByte, HEX);
+    disableMotorsX();
+    Serial.println("X Motor killed");
 
-    // if command is inputting a casette
-    // home the steppers
+    // since motor didnt make it to the position it thought it would, we need to update it with its current position and stop vel/acc commands
+    leftZStepper.stop();
+    rightZStepper.stop();
+    leftZStepper.setCurrentPosition(leftZStepper.currentPosition());
+    rightZStepper.setCurrentPosition(rightZStepper.currentPosition());
 
-    // if command is inputting a new cassette
-    // home the steppers
-    // bring the casette to the position, which is sent by raspberry pi
+    xStepper.stop();
+    xStepper.setCurrentPosition(xStepper.currentPosition());
+
+    delay(50);
+
+    // can just reset all of them
+    topLimLatched = false;
+    btmLimLatched = false;
+    leftLimLatched = false;
+    rightLimLatched = false;
   }
+
+  if(topLimHit)
+  {
+    // switch is being held
+    Serial.println(" top lim is held ");
+  }
+
+  else if(btmLimHit)
+  {
+    Serial.println(" btm lim is held ");
+  }
+
+  else if (leftLimHit)
+  {
+    Serial.println(" left limit is held");
+  }
+
+  else if (rightLimHit)
+  {
+    Serial.println(" right limit is held");
+  }
+
+// TODO: equivalent condition is just the hits, not the latches
+// got rid of btmLimHit condition for sake of the entrance
+  if(!topLimHit && !topLimLatched && !btmLimLatched && !leftLimHit && !leftLimLatched && !rightLimHit && !rightLimLatched)
+  {
+
+    if(Serial.available() > 0)
+    {
+      String incomingMsg = Serial.readStringUntil('\n');
+      incomingMsg.trim();
+
+      if (incomingMsg == "h" && !btmLimHit)
+      {
+        Serial.println("Motor enabled");
+        homeMotorsZ();
+        homeMotorsX();
+      }
+
+      else if (incomingMsg == "c")
+      {
+        Serial.println("Motor enabled");
+        moveTo(entrancePos);
+      }
+
+      // TEMPORARY parsing of the serial message in the format xIndex, zIndex
+      int commaIndex = incomingMsg.indexOf(',');
+
+      if(commaIndex == -1) 
+      {
+        Serial.println("Invalid command format");
+        return;
+      }
+
+      int xIndex = incomingMsg.substring(0, commaIndex).toInt();
+      int zIndex = incomingMsg.substring(commaIndex + 1).toInt();
+
+      // Optional sanity checks
+      if(xIndex < 0 || xIndex > RIGHT_SHELF_IDX || zIndex <= 0 || zIndex > TOP_SHELF_IDX) 
+      {
+        Serial.println("Invalid indices");
+        return;
+      }
+      else 
+      {
+        moveTo(posTranslation(xIndex, zIndex));
+      }
+
+    }
+
+  }
+}
+
+void moveTo(Position posToGoTo)
+{
+  enableMotorsZ();
+  enableMotorsX();
+
+  leftZStepper.moveToDistance(posToGoTo.zPos);
+  rightZStepper.moveToDistance(posToGoTo.zPos);
+  xStepper.moveToDistance(posToGoTo.xPos);
+
+  // check if not at position yet, and also check if any limit has latched at every step
+  while (( leftZStepper.distanceToGo() != 0 || rightZStepper.distanceToGo() != 0 || xStepper.distanceToGo() != 0) && !topLimLatched && !btmLimLatched && !leftLimLatched && !rightLimLatched) 
+  {
+    leftZStepper.run();
+    rightZStepper.run();
+    xStepper.run();
+  }
+
+  Serial.println("Motors killed");
+  disableMotorsZ();
+  disableMotorsX();
+}
+
+void homeMotorsZ()
+{
+
+  //enable the motors to home Z
+  enableMotorsZ();
+
+  // move motors until it hits the limit switch
+  Serial.println(" Homing Motors ");
+  leftZStepper.moveRelative(-1000);
+  rightZStepper.moveRelative(-1000);
+
+  while (!btmLimLatched) 
+  {
+  leftZStepper.run();
+  rightZStepper.run();
+  }
+
+  // kills the two motors
+  Serial.println("Z motors killed");
+  disableMotorsZ();
+
+  // motor doesnt actually move just resets the planner state
+  //resets velocity and accel step 
+  leftZStepper.stop();
+  rightZStepper.stop();
+  delay(50);
+
+  Serial.println(" Zero position set");
+  leftZStepper.setCurrentPosition(0);
+  rightZStepper.setCurrentPosition(0);
+
+  Serial.println(" Backing off from limit switch");
+  delay(500);
+
+  Serial.println("Motor enabled");
+  enableMotorsZ();
+
+  // sometimes it backs off and sometimes it doesnt
+  // TODO: I think this step size could be smaller
+  leftZStepper.moveRelative(2);
+  rightZStepper.moveRelative(2);
+
+  while (( leftZStepper.distanceToGo() != 0 || rightZStepper.distanceToGo() != 0)) 
+  {
+    leftZStepper.run();
+    rightZStepper.run();
+  }
+    
+  btmLimLatched = false;
+
+  // turn motors back off
+  Serial.println("Motor killed");
+  disableMotorsZ();
 
 }
-// functions to make
-// function which takes a positoin, the stepper motor and it moves the stepper there
-// then based on the current position and the positio it has to go to
-// will output the required number of steps, and the direction
-// home the steppers, should be a set position with this code that is called
-// every high to low is one step, the higher the frequency, the faster the 
-// motor spins
 
-// i probably need a zeroing function, in which i drive a stepper motor into 
-// the bottom limit, i then have all the motors killed, i set the z and y pos
-// to zero
-
-/**
-* This arduino receives data from the arduino rev3.
-*
-**/
-void receiveData(int byteCount) 
+void homeMotorsX()
 {
-  // data here will tell us if a casette has entered
-  // if the casette has left as well
-  while (Wire.available()) 
+
+  //enable the motors to home Z
+  enableMotorsX();
+
+  // move motors until it hits the limit switch
+  Serial.println(" Homing X Motor ");
+  xStepper.moveRelative(-1000);
+
+  while (!leftLimLatched) 
   {
-    byte receivedByte = Wire.read();  // Read the incoming byte
-    Serial.print("Received: ");
-    // Print the received byte in hexadecimal
-    Serial.println(receivedByte, HEX);  
+  xStepper.run();
   }
+
+  // kills the two motors
+  Serial.println("x motor killed");
+  disableMotorsX();
+
+  // motor doesnt actually move just resets the planner state
+  //resets velocity and accel step 
+  xStepper.stop();
+  delay(50);
+
+  Serial.println(" Zero position set");
+  xStepper.setCurrentPosition(0);
+
+  Serial.println(" Backing off from limit switch");
+  delay(500);
+
+  Serial.println("X Motor enabled");
+  enableMotorsX();
+
+  // sometimes it backs off and sometimes it doesnt
+  // TODO: I think this step size could be smaller
+  xStepper.moveRelative(4);
+
+  while (xStepper.distanceToGo() != 0) 
+  {
+    xStepper.run();
+  }
+    
+  // just backed off so can change interrupt flag
+  leftLimLatched = false;
+
+  // turn motors back off
+  Serial.println("X Motor killed");
+  disableMotorsX();
   
 }
-void killMotors(){
-// kill all the stepper motors, this means set D13, D9, D5 to high
-// this disables the stepper motor driver, as an emergency stop
-digitalWrite(LEFT_Z_MTR_EN, HIGH);
-digitalWrite(RIGHT_Z_MTR_EN, HIGH);
-digitalWrite(Y_MTR_EN, HIGH);
-// means i should have step down
-// maybe send a message to raspberry pi that there was a collision
+
+void enableMotorsZ()
+{
+  digitalWrite(LEFT_Z_MTR_EN, LOW);
+  digitalWrite(RIGHT_Z_MTR_EN, LOW);
 }
+
+void disableMotorsZ()
+{
+  digitalWrite(LEFT_Z_MTR_EN, HIGH);
+  digitalWrite(RIGHT_Z_MTR_EN, HIGH);
+}
+
+void enableMotorsX()
+{
+  digitalWrite(X_MTR_EN, LOW);
+}
+
+void disableMotorsX()
+{
+  digitalWrite(X_MTR_EN, HIGH);
+}
+
+void killMotors()
+{
+    if (digitalRead(Z_TOP_LIMIT) == LOW) 
+    {
+        topLimLatched = true;
+    }
+
+    else if (digitalRead(Z_BTM_LIMIT) == LOW) 
+    {
+        btmLimLatched = true;
+    }
+
+    else if (digitalRead(X_LEFT_LIMIT) == LOW)
+    {
+      leftLimLatched = true;
+    }
+
+    else if (digitalRead(X_RIGHT_LIMIT) == LOW)
+    {
+      rightLimLatched = true;
+    }
+}
+
+Position posTranslation(int xIdx, int zIdx)
+{
+  Position shelf;
+  shelf.xPos = X_OFFSET + xIdx * X_SHELF_DIST;
+  shelf.zPos = Z_OFFSET + zIdx * Z_SHELF_DIST;
+  return shelf;
+}
+
+
